d733573e41f9332f6b3ae021ca7e3c5f1aa109b9
hnaohiro <hasegawanaohiro1989@gmail.com>
2018-07-23 17:06:57 +0900

    copy client code

---
+++ dockyard-client/src/router.js
@@ -0,0 +1,86 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Club from './views/Club.vue'
+import Ticket from './views/Ticket.vue'
+import Purchase from './views/Purchase.vue'
+import Thanks from './views/Thanks.vue'
+import Mypage from './views/Mypage.vue'
+import Pass from './views/Pass.vue'
+import Login from './views/Login.vue'
+import User from './views/User.vue'
+
+import Qr from './views/shop/Qr.vue'
+import DisplayUser from './views/shop/DisplayUser.vue'
+import EntranceSuccess from './views/shop/EntranceSuccess.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+
+    {
+      path: '/qr',
+      name: 'qr',
+      component: Qr
+    },
+
+    {
+      path: '/displayUser',
+      name: 'displayUser',
+      component: DisplayUser
+    },
+
+    {
+      path: '/entranceSuccess',
+      name: 'entranceSuccess',
+      component: EntranceSuccess
+    },
+
+    {
+      path: '/club',
+      name: 'club',
+      component: Club
+    },
+    {
+      path: '/ticket',
+      name: 'ticket',
+      component: Ticket
+    },
+    {
+      path: '/purchase',
+      name: 'purchase',
+      component: Purchase
+    },
+    {
+      path: '/thanks',
+      name: 'thanks',
+      component: Thanks
+    },
+    {
+      path: '/mypage',
+      name: 'mypage',
+      component: Mypage
+    },
+    {
+      path: '/pass',
+      name: 'pass',
+      component: Pass
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: User
+    }
+  ]
+})
